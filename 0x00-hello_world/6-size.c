62cdeecec403b4c0de0f22504dfcd27ddb553cb2
Amubieya <107006879+Amubieya@users.noreply.github.com>
2022-07-02 04:22:09 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - Entry point
+*Decription: Printing the sizes of variables
+*Return: always zero on success
+*/
 int main(void)
 {
-		int a;
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longSize = sizeof(long int);
+	int longlongSize = sizeof(long long int);
+	int floatSize = sizeof(float);
 
-		long int b;
-
-		long long int c;
-
-		char d;
-
-		float f;
-
-
-		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-
-		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-
-		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-
-		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-
-		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-
-		return (0);
-
+	printf("Size of a char: %d byte(s)\n", charSize);
+	printf("Size of an int: %d byte(s)\n", intSize);
+	printf("Size of a long int: %d byte(s)\n", longSize);
+	printf("Size of a long long int: %d byte(s)\n", longlongSize);
+	printf("Size of a float: %d byte(s)\n", floatSize);
+	return (0);
 }
-
