bdcc7992b0c0b8fbee2cbde73e678bb40898df46
Amubieya <107006879+Amubieya@users.noreply.github.com>
2022-07-15 07:27:23 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,14 +6,11 @@
 * @s2: second string
 * Return: 0 if s1 and s2 are equals
 */
-
 int _strcmp(char *s1, char *s2)
 {
 int i, n;
 i = 0;
-
 n = 0;
-
 while (n == 0)
 {
 if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
