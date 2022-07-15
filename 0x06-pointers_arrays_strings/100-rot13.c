e20b2ddb440cd408d6bb6b19906f06c57e11c495
Amubieya <107006879+Amubieya@users.noreply.github.com>
2022-07-15 07:05:57 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,24 +7,24 @@
 */
 char *rot13(char *s)
 {
-	int count = 0, i;
+int count = 0, i;
 
-	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 52; i++)
-		{
-			if (*(s + count) == letters[i])
-			{
-				*(s + count) = rot13[i];
-				break;
-			}
-		}
-		count++;
-	}
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 52; i++)
+{
+if (*(s + count) == letters[i])
+{
+*(s + count) = rot13[i];
+break;
+}
+}
+count++;
+}
 
-	return (s);
+return (s);
 }
