60a01c47efb09f090aff33fa3273dfc3fdf8bb46
Amubieya <107006879+Amubieya@users.noreply.github.com>
2022-07-15 06:44:31 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,8 @@
-int _putchar(char c);
+#include <unistd.h>
+#ifndef _main_H_
+#define _main_H_
+
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,4 +15,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int main(void);
+
+#endif
