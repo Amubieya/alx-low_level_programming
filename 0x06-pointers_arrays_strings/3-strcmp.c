235352851094cb43b5c75fbc6bc473af3d9b64bf
Amubieya <107006879+Amubieya@users.noreply.github.com>
2022-07-15 07:24:23 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, n;
+int i, n;
 i = 0;
 
-	n = 0;
+n = 0;
 
-	while (n == 0)
-	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		n = *(s1 + i) - *(s2 + i);
-		i++;
-	}
-	return (n);
+while (n == 0)
+{
+if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+break;
+n = *(s1 + i) - *(s2 + i);
+i++;
+}
+return (n);
 }
