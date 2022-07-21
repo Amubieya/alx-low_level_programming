061d4bfcdb4e131864cba014f7589a5cc984786a
Amubieya <107006879+Amubieya@users.noreply.github.com>
2022-07-21 14:40:20 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,23 +7,25 @@
 *Description: finds number of bytes required to find accept in s
 *Return: bytes required to find accept
 */
+
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j, ex;
-for (i = 0; s[i] != '\0'; i++)
-{
-ex = 0;
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (accept[j] == s[i])
-{
-ex = 1;
-}
-}
-if (ex == 0)
-{
-return (i);
-}
-}
-return (i);
+	int i, j, ex;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		ex = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				ex = 1;
+			}
+		}
+		if (ex == 0)
+		{
+			return (i);
+		}
+	}
+	return (i);
 }
