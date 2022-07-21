20f581625de404633c2261a0ceda49bdc482e1bb
amubieya <emmanuelamubieya@gmail.com>
2022-07-21 05:39:14 -0700

    0-puts_recursion.c task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-int i;
-
-i = 0;
-
+int i;	
+i = 0;	
 if (s[i] != '\0')
 {
 _putchar(s[i]);
-_puts_recursion(&s[i + 1]};
+_puts_recursion(&s[i + 1]);
 }
 else
 _putchar('\n');
